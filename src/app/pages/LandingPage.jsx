088e23e33df504bea09dbd441017e784641c2371
aqdhasali <aqdhas@commercializer.global>
2025-05-08 11:58:24 +0530

    improvements

--- src/app/pages/LandingPage.jsx
+++ src/app/pages/LandingPage.jsx
@@ -1,18 +1,32 @@
 export default function LandingPage(){
     return(
-        <div className="flex h-screen">
-            <div className="flex items-center p-5 justify-center flex-col lg:w-1/2">
+        <div className="relative flex h-screen">
+            <div
+                className="absolute inset-0"
+                style={{
+                    backgroundImage: "url('bg.png')",
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    opacity: 0.1, 
+                    zIndex: 0,
+                }}
+            ></div>
+
+            <div className="relative z-10 flex justify-center items-center p-5 flex-col lg:w-1/2">
                 <img className="w-48" src="logo.png" alt="Coco Ease Logo"/>
-                <h1 className="mt-4 text-gray-400 text-2xl font-bold">Growing Strong Together</h1>
-                <h1 className="text-center font-semibold animate-pulse text-4xl mt-24">Launching Soon!</h1>
-                <h1 className="mt-36 text-center p-2">Have any questions or need more info? Reach out to our sales team today!</h1>
-                <h1 className="mt-5 text-gray-600">sales@cocoease.com</h1>
-                <h1 className="text-gray-600">www.cocoease.com</h1>
+                <h1 className="mt-4 text-[#343434] text-2xl font-bold">Growing Strong Together</h1>
+                <h1 className="mt-10 text-center text-[#167A15] font-semibold text-xl">
+                    Delivering high-quality coco substrates and <br/>coir fibre products that are tailored to meet<br/> the needs of commercial growers.
+                </h1>
+                <h1 className="mt-10 text-center p-2">
+                    Have any questions or need more info?<br/>Reach out to our sales team today!
+                </h1>
+                <h1 className="mt-5">sales@cocoease.com</h1>
+                <h1>www.cocoease.com</h1>
             </div>
 
-            
-            <img className="w-1/2 hidden lg:block" src="bg.png"/>
-            
+            <img className="w-1/2 hidden rounded-xl m-5 lg:block relative z-10" src="bg.png"/>
         </div>
+
     )
 }
